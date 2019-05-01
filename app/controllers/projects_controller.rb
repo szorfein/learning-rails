ad0e4882e3f163f80d3b45dc10ab125319930c93
szorfein <szorfein@protonmail.com>
2019-05-01 10:51:48 +0500

    project model

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,20 +1,31 @@
 class ProjectsController < ApplicationController
+
   def index
   end
 
   def new
+    @project = Project.new
   end
 
+  # POST request
   def create
     @project = Project.new(project_params)
-
-    respond_to do |format|
-      if @project.save
-        flash[:notice] = 'Project was successfully created.'
-        redirect_to @project
-      else
-        # nothing, yet
-      end
+    if @project.save
+      flash[:notice] = 'Project was successfully created.'
+      redirect_to @project
+    else
+      # nothing, yet
     end
   end
+
+  def show 
+    @project = Project.find(params[:id])
+  end
+
+end
+
+private
+
+def project_params
+  params.require(:project).permit(:name, :description)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190426002548) do
+ActiveRecord::Schema.define(version: 20190501030648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
