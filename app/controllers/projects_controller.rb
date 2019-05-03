c1a118e2105a7b2bf884d45f83d177132362e7ee
szorfein <szorfein@protonmail.com>
2019-05-03 03:32:42 -0400

    test fail project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,12 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show]
 
   def index
   end
 
   def new
     @project = Project.new
+    @categories = Category.all
   end
 
   # POST request
@@ -16,19 +18,21 @@ class ProjectsController < ApplicationController
         format.html { redirect_to @project, notice: 'Project has been created.' }
       else
         format.html { render :new }
-        flash.now[:alert] = "Project has not been created."
       end
     end
   end
 
   def show 
-    @project = Project.find(params[:id])
   end
 
 end
 
 private
 
+def set_project
+  @project = Project.find(params[:id])
+end
+
 def project_params
-  params.require(:project).permit(:name, :description)
+  params.require(:project).permit(:name, :description, :category_id)
 end
--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -23,7 +23,7 @@ RSpec.feature "Users can create new projects" do
 
   scenario "when providing invalid attributes" do
     click_button "Create Project"
-    expect(page).to have_content "Project has not been created."
+    expect(page).to have_content "error prohibited"
     expect(page).to have_content "Name can't be blank"
   end
 end
