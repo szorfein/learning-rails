d2ecf8da97dd99b480a4a70f3728ef22ae027bdf
szorfein <szorfein@protonmail.com>
2019-06-08 23:13:07 +1030

    admins can edit and update users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,18 @@
 class Admin::UsersController < Admin::ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.order(:email)
   end
 
+  def show 
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
   end
 
   def create
@@ -12,7 +20,7 @@ class Admin::UsersController < Admin::ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to admin_users_path, notice: "User has been created." }
+        format.html { redirect_to admin_users_path, notice: 'User has been created.' }
       else
         flash.now[:alert] = "User has not been created."
         render "new"
@@ -20,7 +28,27 @@ class Admin::UsersController < Admin::ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+
+      if params[:user][:password].blank?
+        params[:user].delete(:password)
+      end
+
+      if @user.update(user_params)
+        format.html { redirect_to admin_users_path, notice: "User has been updated." }
+      else
+        flash.now[:alert] = "User has not been updated."
+        render "edit"
+      end
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:email, :password, :admin)
